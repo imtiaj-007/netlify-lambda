277d4bdcd706b940d157ec3d0e601ecccd7f1f20
SK Imtiaj Uddin <imtiaj.kol@gmail.com>
2024-05-20 21:04:53 +0530

    path fixes

--- controllers/bills.js
+++ controllers/bills.js
@@ -298,7 +298,7 @@ const sendPDF = async (req, res) => {
 
 const generatePdf = async (filePath, billObj, userObj) => {
     try {
-        const htmlTemplate = fs.readFileSync(path.resolve('./views/billTemplate.html'), 'utf8');
+        const htmlTemplate = fs.readFileSync(path.resolve('../views/billTemplate.html'), 'utf8');
         const html = htmlTemplate.replace(/{{customerId}}/g, userObj._id)
             .replace(/{{customerName}}/g, userObj.customerName)
             .replace(/{{email}}/g, userObj.email)
