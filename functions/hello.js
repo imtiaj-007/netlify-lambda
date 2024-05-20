55789efad6973b8f4ab43cbd6a880d1f74ccc748
SK Imtiaj Uddin <imtiaj.kol@gmail.com>
2024-05-20 12:42:41 +0530

    3rd commit

--- functions/hello.js
+++ functions/hello.js
@@ -1,21 +1,18 @@
-exports.handler = async (event, context) => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "Hello, World!" })
-    };
-};
+const express = require('express');
+const serverless = require('serverless-http');
+
+const app = express();
+
+app.get('/hello', (req, res) => {
+  res.json({ message: 'Hello, World!' });
+});
+
+module.exports.handler = serverless(app);
 
 
-
-// // functions/hello.js
-
-// const express = require('express');
-// const serverless = require('serverless-http');
-
-// const app = express();
-
-// app.get('/hello', (req, res) => {
-//   res.json({ message: 'Hello, World!' });
-// });
-
-// module.exports.handler = serverless(app);
+// exports.handler = async (event, context) => {
+//     return {
+//         statusCode: 200,
+//         body: JSON.stringify({ message: "Hello, World!" })
+//     };
+// };
