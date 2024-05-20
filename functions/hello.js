5ec015e646d3bda1715482b06bfef897aa343183
SK Imtiaj Uddin <imtiaj.kol@gmail.com>
2024-05-20 12:36:59 +0530

    2nd try

--- functions/hello.js
+++ functions/hello.js
@@ -1,12 +1,21 @@
-// functions/hello.js
+exports.handler = async (event, context) => {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "Hello, World!" })
+    };
+};
 
-const express = require('express');
-const serverless = require('serverless-http');
 
-const app = express();
 
-app.get('/hello', (req, res) => {
-  res.json({ message: 'Hello, World!' });
-});
+// // functions/hello.js
 
-module.exports.handler = serverless(app);
+// const express = require('express');
+// const serverless = require('serverless-http');
+
+// const app = express();
+
+// app.get('/hello', (req, res) => {
+//   res.json({ message: 'Hello, World!' });
+// });
+
+// module.exports.handler = serverless(app);
